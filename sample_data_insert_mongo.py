308110ac6554a129f774df7651399782f1e11290
Milan OS <milan.s@thinkpalm.com>
2023-07-20 10:51:48 +0530

    modified

--- sample_data_insert_mongo.py
+++ sample_data_insert_mongo.py
@@ -14,11 +14,12 @@ users = [
     {'id': 5, 'name': 'David'},
 ]
 posts= [
-    {'id': 1, 'post_title': 'First Post', 'post_date': '2023-05-30', 'post_summary': 'This is the summary of the first post.'},
-    {'id': 2, 'post_title': 'Second Post', 'post_date': '2023-05-29', 'post_summary': 'This is the summary of the second post.'},
-    {'id': 3, 'post_title': 'Third Post', 'post_date': '2023-05-28', 'post_summary': 'This is the summary of the third post.'},
-    {'id': 4, 'post_title': 'Fourth Post', 'post_date': '2023-05-27', 'post_summary': 'This is the summary of the fourth post.'},
-    {'id': 5, 'post_title': 'Fifth Post', 'post_date': '2023-05-26', 'post_summary': 'This is the summary of the fifth post.'}
+    {'id': 1, 'post_title': 'First Post', 'post_date': '2023-05-30', 'post_img':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3lxIXAVkFS3E_NEAGTw-QH5vMWJe4_yBMDw&usqp=CAU', 'post_summary': 'This is the summary of the first post.'},
+    {'id': 2, 'post_title': 'Second Post', 'post_date': '2023-05-29', 'post_img':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcmkC7A-7K6-PWjzCmzFKKCczWwz4zhb9xKQ&usqp=CAU', 'post_summary': 'This is the summary of the second post.'},
+    {'id': 3, 'post_title': 'Third Post', 'post_date': '2023-05-28', 'post_img':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcmkC7A-7K6-PWjzCmzFKKCczWwz4zhb9xKQ&usqp=CAU', 'post_summary': 'This is the summary of the third post.'},
+    {'id': 4, 'post_title': 'Fourth Post', 'post_date': '2023-05-27', 'post_img':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcmkC7A-7K6-PWjzCmzFKKCczWwz4zhb9xKQ&usqp=CAU', 'post_summary': 'This is the summary of the fourth post.'},
+    {'id': 5, 'post_title': 'Fifth Post', 'post_date': '2023-05-26', 'post_img':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK-BTTAYyTjgBH4IjZy50ceD2IXufgGLy4bQ&usqp=CAU', 'post_summary': 'This is the summary of the fifth post.'},
+
 ]
 def insert_user():
     if user_collection.count_documents({}) == 0:
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify,make_response
 from pymongo import MongoClient
 from bson import json_util
 import json
+from flask_cors import CORS
 
 
 client = MongoClient('localhost',27017)
@@ -12,12 +13,18 @@ event_collection=db['events']
 
 
 app= Flask(__name__)
+cors=CORS(app)
 
 @app.route('/events',methods=['GET'])
 def get_events():
-    events = list(event_collection.find({},{'_id':0,'id':1,'post_title':1,'post_date':1,'post_summary':1}))
+    events = list(event_collection.find({},{'_id':0,'id':1,'post_title':1,'post_date':1,'post_summary':1,'post_img':1}))
     print(events)
     return jsonify(events)
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0", port = 8000)
+
+
+# @app.route('/events',methods=['POST'])
+# def add_events():
+#     events = list(event_collection)
