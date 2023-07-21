9a58690ae7f3ffc80afe51906c11b7b3666f0655
divin_thomas <divin.thomas@cohesity.com>
2023-07-21 04:24:08 -0700

    add post_data

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify,make_response
+from flask import Flask, jsonify,make_response,request
 from pymongo import MongoClient
 from bson import json_util
 import json
 from flask_cors import CORS
-
+from jsonschema import validate
 
 client = MongoClient('localhost',27017)
 db=client['EventPlanner']
@@ -15,23 +15,47 @@ event_collection=db['events']
 app= Flask(__name__)
 cors=CORS(app)
 
+event_schema={
+    "type": "object",
+    "properties": {
+        "start_date": {"type": "string", "format": "date"},
+        "end_date": {"type": "string", "format": "date"},
+        "title": {"type": "string"},
+        "description": {"type": "string"},
+        "author": {"type": "string"},
+    },
+    "required": ["start_date", "end_date", "title"]
+}
+
+
 @app.route('/events',methods=['GET'])
 def get_events():
     events = list(event_collection.find({},{'_id':0,'id':1,'post_title':1,'post_date':1,'post_summary':1,'post_img':1}))
     print(events)
     return jsonify(events)
 
-@app.route('/sign-up', method=['GET'])
-def sign_up():
-    pass
+@app.route('/events',methods=['POST'])
+def post_events():
+    data= request.get_json()
+    try:
+        validate(instance=data, schema=event_schema)
+    except jsonschema.exceptions.ValidationError as e:
+        return jsonify({'error': 'Invalid event data', 'details': str(e)}), 400
+    #print(data)
+    update = event_collection.insert_one(data)
+    return (jsonify('success')),201
 
-@app.route('/login', method=['GET'])
-def login():
-    pass
+# @app.route('/sign-up', method=['GET'])
+# def sign_up():
+#     pass
 
-@app.route('/login', method=['GET'])
-def logout():
-    pass
+# @app.route('/login', method=['GET'])
+# def login():
+#     pass
+
+# @app.route('/login', method=['GET'])
+# def logout():
+#     pass
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0", port = 8000)
